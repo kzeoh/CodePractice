332f0776cd0fc30a8d5755e3b97a37b034bb975b
kzeoh <kwonje.oh2@gmail.com>
2020-01-20 18:47:12 +0900

    start 1012

---
+++ 1012/1012-cabbable.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int map[50][50];
+int visited[50][50];
+int M, N, K, X, Y;
+
+void dfs(int node){
+	
+}
+
+
+int main(){
+	
+	int T;
+
+	cin >> T;
+	for(int j=0; j<T;j++){
+		cin >> M >> N >> K;
+
+		for(int i=0;i<K;i++){
+			cin	>> X, Y;
+			map[X][Y]=1;
+		}
+		for(int i=0;
+		dfs(i);
+	}
+
+
+
+	return 0;
+}
