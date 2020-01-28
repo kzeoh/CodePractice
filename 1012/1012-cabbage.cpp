5d6b56588c77141ed43bb82d69dcef7d41e5f43a
kzeoh <kwonje.oh2@gmail.com>
2020-01-28 10:38:52 +0900

    1204-init

--- 1012/1012-cabbage.cpp
+++ 1012/1012-cabbage.cpp
@@ -6,14 +6,27 @@ int map[50][50];
 int visited[50][50];
 int M, N, K, X, Y;
 
-void dfs(int node){
-	
+int dfs(int dx, int dy){
+	int ret;
+
+	if(map[i+1][j]==1){
+		
+
+	}else(map[i][j+1]==1){
+
+	}
+		visited[i][j]=1;
+		dfs(i,j);
+
+	return ret;
 }
 
 
 int main(){
 	
 	int T;
+	int ret;
+	int cnt;
 
 	cin >> T;
 	for(int j=0; j<T;j++){
@@ -23,8 +36,15 @@ int main(){
 			cin	>> X, Y;
 			map[X][Y]=1;
 		}
-		for(int i=0;
-		dfs(i);
+		for(int i=0;i<M-1;i++){
+			for(int k=0;i<N-1;k++){
+				if(map[i][k]==1&&visited[i][k]==0){
+					ret=dfs(i,k);
+					if(ret)
+						cnt++;			
+				}
+			}
+		}
 	}
 
 
---
+++ 1204/1204-most.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+ 
+using namespace std;
+ 
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
+    int a[1001];
+    /*
+       여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+    */
+    for(test_case = 1; test_case <= 10; ++test_case)
+    {
+        int n;
+		int temp = 0;
+		int max = 0;
+		int maxVal=0;
+        cin>>n;
+//      cout<<n;
+        for(int i=0; i < 1000; i++){
+            cin>>temp;
+			a[temp]++;
+			if((a[temp]>=max)&&(temp>maxVal)){
+				max=a[temp];
+				maxVal=temp;
+			}
+        }
+ 
+        cout<<'#'<<test_case<<' '<<maxVal<<'\n';
+ 
+    }
+    return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
