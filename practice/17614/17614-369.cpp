792afd1b9c0f6f5ca99b9f8f181e97f8966e4cc6
kzeoh <kwonje.oh2@gmail.com>
2020-01-30 23:37:08 +0900

    add practice

---
+++ practice/17614/17614-369.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+	int n;
+	string s;
+	char c;
+	int flag=0;
+	int cnt=0;
+
+	cin >> n;
+
+	for(int i=1;i<=n;i++){
+		s = to_string(i);
+		for(int j=0; j<s.size();j++){
+			c=s.at(j);
+			if(c=='3')
+				cnt++;
+			else if(c=='6')
+				cnt++;
+			else if(c=='9')
+				cnt++;
+		}
+		
+	}
+	cout << cnt;
+
+	return 0;
+}
