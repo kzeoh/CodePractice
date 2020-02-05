c23d04255427e90134a0092e75fb132b4a6a187f
kzeoh <kwonje.oh2@gmail.com>
2020-02-05 16:14:09 +0900

    fix-17615

--- practice/17615/17615-ball.cpp
+++ practice/17615/17615-ball.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -8,71 +9,105 @@ int main(){
 
 	int n;
 	int Rcnt =0, Bcnt=0;
-	int minCnt=2e9;
+	int minCnt=2e9, minCnt2=2e9;
 	int flag=0;
 	vector <char> red, blue;
 	char c,temp;
 	string s;
 
 	cin >> n >> s;
-	cout << n << s << "\n";
+//	cout << n << s << "\n";
 	c=s.at(0);
-	
-	for(int i=1; i < n; i++){
+	if(c!=s.at(1)&&c=='R'){
+		flag=1;
+	}else if(c==s.at(1)&&c=='B'){
+		flag=1;
+	}else if(c=='B'&&s.at(1)=='R'){
+		flag=1;
+		Rcnt++;
+	}
+
+	for(int i=2;i<n;i++){
 		temp=s.at(i);
-		if(c!=temp){
-			if(flag){
-				flag=0;
-				if(c=='R')
-					Rcnt++;
-				else
-					Bcnt++;
-			}else{
-				if(c=='R')
-					Rcnt++;
-				else
-					Bcnt++;
-					
-			}
-			continue;
-		}
-		if(c==temp){
-			if(!flag)
-				flag=1;
-			continue;
+		if(temp=='R'&&flag){
+			Rcnt++;
+		}else if(temp=='B'&&!flag){
+			flag=1;
 		}
 	}
-	cout << Rcnt << Bcnt << "\n";	
-	minCnt=min(Rcnt,Bcnt);	
-
-	c=s.at(n-1);
 	flag=0;
-	Rcnt=0;
-	Bcnt=0;
-	for(int i=n-2; i >=0; i--){
+	if(c!=s.at(1)&&c=='B'){
+		flag=1;
+	}else if(c==s.at(1)&&c=='R'){
+		flag=1;
+	}else if(c=='R'&&s.at(1)=='B'){
+		flag=1;
+		Bcnt++;
+	}
+
+
+	for(int i=2;i<n;i++){
 		temp=s.at(i);
-		if(c!=temp){
-			if(flag){
-				flag=0;
-				if(c=='R')
-					Rcnt++;
-				else
-					Bcnt++;
-			}
-			continue;
-		}
-		if(c==temp){
-			if(!flag)
-				flag=1;
-			continue;
+		if(temp=='B'&&flag){
+			Bcnt++;
+		}else if(temp=='R'&&!flag){
+			flag=1;
 		}
 	}
 	
-	if(minCnt<min(Rcnt,Bcnt))
-		minCnt=min(Rcnt,Bcnt);
-	cout << Rcnt << Bcnt << "\n";	
+//	cout << Rcnt << Bcnt << "\n";	
+	minCnt=min(Rcnt,Bcnt);	
+	Rcnt =0;
+	Bcnt =0;
+	flag=0;
+	c=s.at(n-1);
+//	cout<<c<<"\n";
+	if(c!=s.at(n-2)&&c=='R'){
+		flag=1;
+	}else if(c==s.at(n-2)&&c=='B'){
+		flag=1;
+	}else if(c=='B'&&s.at(n-2)=='R'){
+		flag=1;
+		Rcnt++;
+	}
+//	cout<< Rcnt <<" "<<flag<< "\n";
+	for(int i=n-3;i>=0;i--){
+		temp=s.at(i);
+		if(temp=='R'&&flag){
+			Rcnt++;
+		}else if(temp=='B'&&!flag){
+			flag=1;
+		}
+	}
+//	cout<< Rcnt <<"\n";
+	flag=0;
+	if(c!=s.at(n-1)&&c=='B'){
+		flag=1;
+	}else if(c==s.at(n-2)&&c=='R'){
+		flag=1;
+	}else if(c=='R'&&s.at(1)=='B'){
+		flag=1;
+		Bcnt++;
+	}
 
-	cout << minCnt;
+
+	for(int i=n-3;i>=0;i--){
+		temp=s.at(i);
+		if(temp=='B'&&flag){
+			Bcnt++;
+		}else if(temp=='R'&&!flag){
+			flag=1;
+		}
+	}
+	
+//	cout << Rcnt << Bcnt << "\n";	
+	minCnt2=min(Rcnt,Bcnt);	
+
+	minCnt=min(minCnt,minCnt2);
+	
+	
+
+	cout << minCnt<<"\n";
 		
 	return 0;
 }
