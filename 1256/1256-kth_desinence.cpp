e99e4577b989be8148210cc8bbe54081cf8ef3f7
kzeoh <kwonje.oh2@gmail.com>
2020-03-30 16:57:04 +0900

    finish-1256

---
+++ 1256/1256-kth_desinence.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	vector <string> v;
+	int tc;
+	string s;
+
+	cin >> tc;
+
+	for (int i = 1;i <= tc;i++) {
+		int N;
+		cin >> N;
+		cin >> s;
+
+		v.push_back(s);
+		for (int j = 1;j < s.size();j++) {
+			v.push_back(s.substr(j));
+		}
+		
+		sort(v.begin(), v.end());
+		cout << "#" << tc << " " << v[N-1]<< "\n";
+
+		v.clear();	
+	}
+
+
+
+	return 0;
+}
