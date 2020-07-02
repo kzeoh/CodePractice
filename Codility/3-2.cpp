c179d6ef3a0ac04d0ccdfe95b022a920c9b8fd8d
kzeoh <kwonje.oh2@gmail.com>
2020-07-03 00:04:23 +0900

    Codility3-2 100%

--- Codility/3-2.cpp
+++ Codility/3-2.cpp
@@ -1,32 +1,26 @@
 #include <algorithm>
+#include <set>
 
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
 int solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
-    int answer=0;
+    int answer=1;
     int count=0;
-    if(A.size()==1){
-        answer=++A[0];
-        return answer;
-    }else if(A.size()==0){
-        answer=1;
-        return answer;
+    set<int> s;
+    
+    for(int i=0;i<A.size();i++){
+        s.insert(A[i]);    
     }
-    sort(A.begin(),A.end());
-    count=A[0];
-    //cout << count;
-    for(int i=1;i<A.size();i++){
-        count++;
-        if(A[i]!=count){
-            answer=count;
+    
+    for(int i=1;i<=A.size()+1;i++){
+        if(s.count(i)==0){
+            answer=i;
             break;
         }
     }
     
-
-    //cout<<answer;
-    
     return answer;
 }
+
