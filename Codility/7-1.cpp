5642ae264873b3b8b57de30846031034b8f20b3d
kzeoh <kwonje.oh2@gmail.com>
2020-07-03 23:49:06 +0900

    Codility 7-1 62%

---
+++ Codility/7-1.cpp
@@ -0,0 +1,38 @@
+// you can use includes, for example:
+// #include <algorithm>
+#include <stack>
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(string &S) {
+    // write your code in C++14 (g++ 6.2.0)
+    
+    stack <char> s;
+    
+    for(int i=0;i<S.size();i++){
+        if(S[i]=='{'||S[i]=='('||S[i]=='[')
+            s.push(S[i]);
+        else{
+            if(S[i]=='}'){
+                if(s.top()!='{')
+                    return 0;
+                else
+                    s.pop();
+            }else if(S[i]==')'){
+                if(s.top()!='(')
+                    return 0;
+                else
+                    s.pop();
+            }else if(S[i]==']'){
+                if(s.top()!='[')
+                    return 0;
+                else
+                    s.pop();
+            }
+        }
+    }
+    
+    return 1;
+    
+}
+
