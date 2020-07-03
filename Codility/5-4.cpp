032996ddbd99babbccb7a626a23ded0e168160d6
kzeoh <kwonje.oh2@gmail.com>
2020-07-03 16:47:41 +0900

    Codility 5-4 70%

---
+++ Codility/5-4.cpp
@@ -0,0 +1,20 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    int count=0;
+    int answer=0;
+    for(int i = A.size()-1;i>=0;i--){
+        if(A[i]){
+            count++;
+        }else
+            answer+=count;
+    }
+    
+    return answer;
+}
+
