a5d8d9886bd18221a3ac85c22da759eb976c3a87
kzeoh <kwonje.oh2@gmail.com>
2020-07-03 23:27:22 +0900

    Codility 6-4 81%

---
+++ Codility/6-4.cpp
@@ -0,0 +1,19 @@
+// you can use includes, for example:
+#include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    sort(A.begin(),A.end());
+    
+    for(int i=0;i<A.size()-2;i++){
+        if(A[i]+A[i+1]>A[i+2]){
+            return 1;
+        }
+    }
+    return 0;
+    
+}
+
