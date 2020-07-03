19a5739bcc60664e2094a4eaa35d728c5667404d
kzeoh <kwonje.oh2@gmail.com>
2020-07-03 23:32:04 +0900

    Codility 6-4 93%

--- Codility/6-4.cpp
+++ Codility/6-4.cpp
@@ -6,6 +6,9 @@
 
 int solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
+    if(A.size()<3)
+        return 0;
+    
     sort(A.begin(),A.end());
     
     for(int i=0;i<A.size()-2;i++){
