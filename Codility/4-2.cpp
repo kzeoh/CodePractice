071d9301e17888cde9914cc2c7448764c4360faa
kzeoh <kwonje.oh2@gmail.com>
2020-07-03 13:13:37 +0900

    Codility 4-2 100%

--- Codility/4-2.cpp
+++ Codility/4-2.cpp
@@ -1,6 +1,7 @@
 // you can use includes, for example:
 #include <algorithm>
 #include <vector>
+#include <map>
 
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
@@ -8,23 +9,41 @@
 vector<int> solution(int N, vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
     vector <int> v(N+1);
+    //map <int, int> m;
+    int m[N+1]={0,};
+    int count =0;
+    
     int maxVal=0;
+    int inc=0;
     
     for(int i =0;i<A.size();i++){
         if(N+1==A[i]){
-            for(int j=1;j<v.size();j++){
+            count++;
+            inc=maxVal;
+            /*for(int j=1;j<v.size();j++){
                 v[j]=maxVal;
-            }
+            }*/
         }else{
-            v[A[i]]++;
+            if(m[A[i]]==count)
+                v[A[i]]++;
+            else{
+                m[A[i]]=count;
+                v[A[i]]=inc+1;
+            }
+            
             if(v[A[i]]>maxVal)
                 maxVal=v[A[i]];
         }
         // for(int j=1;j<v.size();j++){
         //         cout<< v[j]<<" ";
         // }
-        //cout<<"\n";
+        // cout<<"\n";
     }
+    for(int j=1;j<v.size();j++){
+           if(m[j]!=count)
+                 v[j]=inc;
+    }
+    
     v.erase(v.begin());
     
     return v;
