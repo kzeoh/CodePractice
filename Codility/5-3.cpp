787e4989b97b169eaa28bf023f7833c57b12229a
kzeoh <kwonje.oh2@gmail.com>
2020-07-03 16:12:44 +0900

    Codility 5-3 (mathematical) 90%

---
+++ Codility/5-3.cpp
@@ -0,0 +1,31 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    
+    int minIdx=0;
+    float minVal=1e9;
+    
+    
+    for(int i=2;i<A.size();i++){
+        float temp = (float)(A[i]+A[i-1]+A[i-2])/(float)3;
+        //cout << temp<<"\n";
+        if(temp<minVal){
+            minVal = temp;
+            minIdx=i-2;
+        }
+        temp = (float)(A[i]+A[i-1])/(float)2;
+       // cout << temp<<"\n";
+        if(temp<minVal){
+            minVal = temp;
+            minIdx=i-1;
+        }
+    }
+    
+    return minIdx;
+}
+
