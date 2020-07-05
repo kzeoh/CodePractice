04d40fa4cad121de6b48549d818d2c3742f2b183
kzeoh <kwonje.oh2@gmail.com>
2020-07-05 19:06:03 +0900

    Codility 11-1 88%

--- Codility/11-1.cpp
+++ Codility/11-1.cpp
@@ -8,7 +8,8 @@
 
 vector<int> solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
-    map<int,int> m;
+    //map<int,int> m;
+    vector<int> m(2*A.size()+1,0);
     vector<int> answer;
     
     for(int i=0;i<A.size();i++){
@@ -19,10 +20,10 @@ vector<int> solution(vector<int> &A) {
         int cnt=0;
         for(int j=1;j<=sqrt(A[i]);j++){
             if(A[i]%j==0){
-                if(m.count(j)){
+                if(m[j]){
                     cnt+=m[j];
                 }
-                if(A[i]/j!=j&&m.count(A[i]/j)){
+                if(A[i]/j!=j&&m[A[i]/j]){
                     cnt+=m[A[i]/j];
                 }
                 
