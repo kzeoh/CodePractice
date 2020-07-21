8bce915db9985b712e06bb51b70f7f422c96bf73
Kwonje Oh <kwonje.oh2@gmail.com>
2020-07-21 17:00:15 +0900

    Codility 3-3 2nd 84%

---
+++ Codility/2nd/3-3.cpp
@@ -0,0 +1,26 @@
+// you can use includes, for example:
+// #include <algorithm>
+#include<cmath>
+// you can write to stdout for debugging purposes, e.g.
+// cout << this is a debug message << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    vector <int> reverseAdd(A.size()+1,0);
+    
+    for(int i=A.size()-1;i>=0;i--){
+        reverseAdd[i]=reverseAdd[i+1]+A[i];
+    }
+    int aggSum =0;
+    int minDiff = 1e9;
+    
+    for(int i=0;i<A.size();i++){
+        aggSum+=A[i];
+        if(abs(aggSum-reverseAdd[i+1])<minDiff){
+            minDiff=abs(aggSum-reverseAdd[i+1]);
+            
+            //cout << reverseAdd[i+1]<< <<minDiff<<  <<answer<<n;
+        }
+    }
+    return minDiff;
+}
