3a299c53e26ceb872ba7dda9a5e131b5beeb5f52
Kwonje Oh <kwonje.oh2@gmail.com>
2020-07-21 16:37:45 +0900

    Codility 3-2 2nd 100%

--- Codility/2nd/3-2.cpp
+++ Codility/2nd/3-2.cpp
@@ -7,14 +7,14 @@
 int solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
     int answer=0;
-    
+
     sort(A.begin(),A.end());
-    
+
     for(int i=0;i<A.size();i++){
         if(A[i]!=i+1){
             return i+1;
         }
     }
-    
+    answer=A.size()+1;
     return answer;
 }
