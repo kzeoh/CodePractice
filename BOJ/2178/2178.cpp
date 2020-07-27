9c56017cddec155e7ac2f7f1395cd4de179763fa
kwonje oh <kwonje.oh2@gmail.com>
2020-07-27 22:17:49 +0900

    BOJ-2178-미로탐색-time reduce in bfs memory reduce

--- BOJ/2178/2178.cpp
+++ BOJ/2178/2178.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 int arr[101][101]={0,};
-int visited[101][101]={0,};
+//int visited[101][101]={0,};
 int coord[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
 int answers[101][101]={0,};
 
-int answer=1e9;
+//int answer=1e9;
 int N, M;
 
 
-void solve(int n, int m, int step){
+/*void solve(int n, int m, int step){
 	if(n==N&&m==M){
 		if(step<answer){
 			answer=step;
@@ -37,23 +37,23 @@ void solve(int n, int m, int step){
 		}				
 	}
 
-}
+}*/
 
 void bfs(int n, int m){
 	queue <pair<int,int>> step;
 	int cnt = 0;
-	bool visit[101][101]={0,};
+//	bool visit[101][101]={0,};
 	step.push({n,m});
 	answers[n][m]=1;
 	while(!step.empty()){
 		n = step.front().first;
 		m = step.front().second;
 		step.pop();
-		visit[n][m]=1;
+//		visit[n][m]=1;
 		for(int i=0;i<4;i++){
 			int nx = n+coord[i][0];
 			int mx = m+coord[i][1];
-			if(!visit[nx][mx]&&arr[nx][mx]==1){
+			if(!answers[nx][mx]&&arr[nx][mx]==1){
 				step.push({nx,mx});
 				answers[nx][mx]=answers[n][m]+1;
 			}
@@ -73,7 +73,7 @@ int main (){
 		}
 	}
 
-	visited[1][1]=1;
+//	visited[1][1]=1;
 	//solve(1,1,1);
 	bfs(1,1);
 //	cout<< answer <<"\n";
